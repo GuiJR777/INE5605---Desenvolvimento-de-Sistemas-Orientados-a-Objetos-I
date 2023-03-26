2d49b18ef7c9d4bf5d034c8b13a12ab700b4ffdb
Guilherme Jesuino Ramires <guilherme.ramires@quintoandar.com.br>
2023-03-26 14:08:31 -0300

    Fix

--- exercicios/exercicio_03/pacote_viagem.py
+++ exercicios/exercicio_03/pacote_viagem.py
@@ -20,46 +20,46 @@ class PacoteViagem:
         else:
             raise TypeError("Custo unitário deve ser um inteiro")
 
-        @property
-        def origem(self) -> str:
-            return self.__origem
+    @property
+    def origem(self) -> str:
+        return self.__origem
 
-        @origem.setter
-        def origem(self, origem: str) -> None:
-            if isinstance(origem, str):
-                self.__origem = origem
-            else:
-                raise TypeError("Origem deve ser uma string")
+    @origem.setter
+    def origem(self, origem: str) -> None:
+        if isinstance(origem, str):
+            self.__origem = origem
+        else:
+            raise TypeError("Origem deve ser uma string")
 
-        @property
-        def destino(self) -> str:
-            return self.__destino
+    @property
+    def destino(self) -> str:
+        return self.__destino
 
-        @destino.setter
-        def destino(self, destino: str) -> None:
-            if isinstance(destino, str):
-                self.__destino = destino
-            else:
-                raise TypeError("Destino deve ser uma string")
+    @destino.setter
+    def destino(self, destino: str) -> None:
+        if isinstance(destino, str):
+            self.__destino = destino
+        else:
+            raise TypeError("Destino deve ser uma string")
 
-        @property
-        def duracao(self) -> int:
-            return self.__duracao
+    @property
+    def duracao(self) -> int:
+        return self.__duracao
 
-        @duracao.setter
-        def duracao(self, duracao: int) -> None:
-            if isinstance(duracao, int):
-                self.__duracao = duracao
-            else:
-                raise TypeError("Duração deve ser um inteiro")
+    @duracao.setter
+    def duracao(self, duracao: int) -> None:
+        if isinstance(duracao, int):
+            self.__duracao = duracao
+        else:
+            raise TypeError("Duração deve ser um inteiro")
 
-        @property
-        def custo_unitario(self) -> int:
-            return self.__custo_unitario
+    @property
+    def custo_unitario(self) -> int:
+        return self.__custo_unitario
 
-        @custo_unitario.setter
-        def custo_unitario(self, custo_unitario: int) -> None:
-            if isinstance(custo_unitario, int):
-                self.__custo_unitario = custo_unitario
-            else:
-                raise TypeError("Custo unitário deve ser um inteiro")
+    @custo_unitario.setter
+    def custo_unitario(self, custo_unitario: int) -> None:
+        if isinstance(custo_unitario, int):
+            self.__custo_unitario = custo_unitario
+        else:
+            raise TypeError("Custo unitário deve ser um inteiro")
